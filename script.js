bfdfbe9f583f5a62d196fa3196397da8537a9606
DevTechG <gabriel10300007@gmail.com>
2025-01-30 15:19:24 -0300

    Adding a new expense to the list.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
+// Seleciona os elementos da lista.
+const expenseList = document.querySelector("ul")
+
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
     // Obtém o valor atual do input e remove os caracteres não numéricos.
@@ -42,5 +45,49 @@ form.onsubmit = (event) => {
         created_at: new Date()
     }
 
-    console.log(newExpense)
+    // Chama a função que ira adicionar o item na lista.
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+        // Cria o elemento para adicionar o item (li) na lista (ul).
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+
+        // Cria o ícone da categora.
+        const expenseIcon = document.createElement("img")
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.category_name)
+
+        // Cria as informações da despesa.
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info")
+
+        // Cria o nome da despesa.
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
+
+        // Cria a categoria da despesa.
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.category_name
+
+        // Adiciona nome e categoria na div das informações da despesa.
+        expenseInfo.append(expenseName,expenseCategory)
+
+        // Cria o valor da despesa.
+        const expenseAmount = document.createElement("span")
+        expenseAmount.classList.add("expense-amount")
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$","")}`
+
+        // Adiciona as informações no item.
+        expenseItem.append(expenseIcon, expenseInfo,expenseAmount)
+
+        // Adiciona o item na lista.
+        expenseList.append(expenseItem)
+
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despensas.")
+        console.log
+    }
 }
