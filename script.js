a51bf33168b49bbdb429d87558d688a7f647f610
DevTechG <gabriel10300007@gmail.com>
2025-01-30 14:29:41 -0300

    Capture the submit event and create an object with details of the new expense.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-// Seleciona os elementos do formulário.
+// Seleciona os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
     // Obtém o valor atual do input e remove os caracteres não numéricos.
-    let value = amount.value.replace(/D/g, "")
+    let value = amount.value.replace(/\D/g, "")
 
     // Transforma o valor em centavos (exemplo: 150/100 = 1.5 que é equivalente a R$ 1,500)
     value = Number(value) / 100
 
     // Atualiza valor do input.
-    amount.value = value
+    amount.value = formatCurrencyBRL(value)
 }
 
 function formatCurrencyBRL(value) {
@@ -22,4 +25,22 @@ function formatCurrencyBRL(value) {
 
     // Retorna o valor formatado.
     return value
+}
+
+// Captura o evento de submit do formulário para obter os valores.
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de recarregar a página.
+    event.preventDefault()
+
+    // Cria um objeto com os detalhes na nova despesa.
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date()
+    }
+
+    console.log(newExpense)
 }
